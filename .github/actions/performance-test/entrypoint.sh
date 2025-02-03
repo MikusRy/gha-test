01d4c6e9d59a9bf2b3d4a14200241f05280ccb02
MikusRy <mikolaj.rychel@gmail.com>
2025-02-03 13:46:10 +0100

    Update entrypoint.sh

--- .github/actions/performance-test/entrypoint.sh
+++ .github/actions/performance-test/entrypoint.sh
@@ -8,12 +8,9 @@ dockerd-entrypoint.sh &
 sleep 5
 
 echo "Running a nested Docker container..."
-docker run --rm busybox echo "Inside the nested container"
+docker run -dit -p 8080:8080 my-image:latest 
 
 echo "Running JMeter Test..."
-jmeter -n -t /app/test.jmx -l /app/results.jtl
+jmeter -n -t /app/jmeter.jmx -l /app/results.jtl
 
 echo "JMeter test completed. Results stored in /app/results.jtl"
-
-# Execute additional commands if provided
-exec "$@"
